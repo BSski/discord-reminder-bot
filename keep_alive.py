204821b1d70dfd8a675ccef980c495db7ca109f3
bsski <50927653+BSski@users.noreply.github.com>
2022-04-28 20:51:07 +0200

    1.1.2 release

---
+++ keep_alive.py
@@ -0,0 +1,24 @@
+"""
+Allows Uptime Robot to periodically connect to bot's replit host, which keeps
+the repository awake continuously.
+"""
+from threading import Thread
+
+from flask import Flask
+
+
+app = Flask("")
+
+
+@app.route("/")
+def home() -> str:
+    return "Hello. I am alive!"
+
+
+def run() -> None:
+    app.run(host="0.0.0.0", port=8080)
+
+
+def keep_alive() -> None:
+    t = Thread(target=run)
+    t.start()
---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+
+from base.utils import reply_to_greeting_with_a_chance
+import global_const
+from keep_alive import keep_alive
+from reminders.reminders_bot import check_reminders
+from reminders.utils import get_commands_prefix
+
+
+bot = commands.Bot(command_prefix=get_commands_prefix, case_insensitive=True)
+bot.remove_command("help")
+
+bot.load_extension("reminders.reminders_bot")
+bot.load_extension("base.base")
+
+
+@bot.event
+async def on_ready() -> None:
+    """
+    Discord.py docs:
+    "Called when the client is done preparing the data received from Discord. Usually after login is successful and the Client.guilds and co. are filled up."
+    """
+    print("Bot is online.")
+    bot.loop.create_task(check_reminders(bot))
+
+
+@bot.event
+async def on_message(message: discord.message.Message) -> None:
+    """
+    Discord.py docs:
+    "Called when a Message is created and sent."
+    """
+    await reply_to_greeting_with_a_chance(message, bot)
+    await bot.process_commands(message)
+
+
+keep_alive()
+bot.run(global_const.TOKEN)
