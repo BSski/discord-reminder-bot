0ff46a1a13109ceed398169466626df7b44186f0
BSski <swedrowskibartosz@gmail.com>
2024-01-29 21:38:23 +0100

    Migrate to discord.py 2

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ intents = discord.Intents.default()
 bot = commands.Bot(command_prefix=get_commands_prefix, case_insensitive=True, intents=intents)
 bot.remove_command("help")
 
-bot.load_extension("reminders.reminders_bot")
-bot.load_extension("base.base")
+try:
+    bot.load_extension("reminders.reminders_bot")
+    bot.load_extension("base.base")
+except commands.errors.ExtensionAlreadyLoaded:
+    print("An extension with the same name is already loaded.")
 
 
 @bot.event
@@ -24,20 +27,7 @@ async def on_ready() -> None:
     print("Bot is online.")
     bot.loop.create_task(check_reminders(bot))
 
-
-# TODO: Establish if can delete this one.
-@bot.event
-async def on_message(message: discord.message.Message) -> None:
-    """
-    // TODO: Update this docstring after migrating to Discord.py 2.0.
-    Called when a Message is created and sent.
-    """
-    await bot.process_commands(message)
-
-
 keep_alive()
 
-try:
-    bot.run(global_const.TOKEN)
-except:
-    print("\nThe bot has shut down due to an error.")
+
+bot.run(global_const.TOKEN)
